bc1c8498a48f30a289bc2d4ceb3565d671142a7f
study <study@lan.sibadi.org>
2022-11-27 20:41:39 +0600

    create VolumeArray and CreateArray

--- task/Program.cs
+++ task/Program.cs
@@ -9,4 +9,23 @@
     PrintArray(resultArray);
 }
 
+int VolumeArray(string[] arr) // определение количества элементов в новом массиве
+{
+    int volumeArray = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i].Length <= 3)
+        {
+            volumeArray++;
+        }
+    }
+    return volumeArray;
+}
+
+string[] CreateArray(int volumeArr) // создание массива для заполнения необходимой длины
+{
+    string[] createArray = new string[volumeArr];
+    return createArray;
+}
+
 Main();
